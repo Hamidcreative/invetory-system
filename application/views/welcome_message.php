27590730815cab06620cbc2076e500efb87937ff
hamidcreative <hamid.creativetech@gmail.com>
2019-04-25 00:22:31 +0500

    Theme settings

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -62,7 +62,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	</div>
 	<ul class="sidenav sidenav-collapsible leftside-navigation collapsible sidenav-fixed menu-shadow" id="slide-out" data-menu="menu-navigation" data-collapsible="accordion">
 		<li class="bold">
-			<a class="collapsible-header waves-effect waves-cyan " href="#"><i class="material-icons">photo_filter</i><span class="menu-title" data-i18n="">Users</span></a>
+			<a class="collapsible-header waves-effect waves-cyan " href="#"><i class="material-icons">face</i><span class="menu-title" data-i18n="">Users</span></a>
 			<div class="collapsible-body">
 				<ul class="collapsible collapsible-sub" data-collapsible="accordion">
 					<li><a class="collapsible-body" href="#" data-i18n=""><i class="material-icons">radio_button_unchecked</i><span> Users</span></a></li>
@@ -71,11 +71,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			</div>
 		</li>
 		<li class="bold">
-			<a class="collapsible-header waves-effect waves-cyan " href="#"><i class="material-icons">photo_filter</i><span class="menu-title" data-i18n="">Warehouses</span></a>
+			<a class="collapsible-header waves-effect waves-cyan " href="#"><i class="material-icons">local_grocery_store</i><span class="menu-title" data-i18n="">Warehouses</span></a>
 			<div class="collapsible-body">
 				<ul class="collapsible collapsible-sub" data-collapsible="accordion">
 					<li><a class="collapsible-body" href="#" data-i18n=""><i class="material-icons">radio_button_unchecked</i><span> Warehouses</span></a></li>
 					<li><a class="collapsible-body" href="#" data-i18n=""><i class="material-icons">radio_button_unchecked</i><span>Add Warehouse</span></a></li>
+				</ul>
+			</div>
+		</li>
+		<li class="bold">
+			<a class="collapsible-header waves-effect waves-cyan " href="#"><i class="material-icons">local_grocery_store</i><span class="menu-title" data-i18n="">Warehouse Types</span></a>
+			<div class="collapsible-body">
+				<ul class="collapsible collapsible-sub" data-collapsible="accordion">
+					<li><a class="collapsible-body" href="#" data-i18n=""><i class="material-icons">radio_button_unchecked</i><span> Warehouse Types</span></a></li>
+					<li><a class="collapsible-body" href="#" data-i18n=""><i class="material-icons">radio_button_unchecked</i><span>Add Warehouse Type</span></a></li>
 				</ul>
 			</div>
 		</li>
@@ -97,8 +106,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 				</ul>
 			</div>
 		</li>
-		<li class="bold"><a class="waves-effect waves-cyan " href="#"><i class="material-icons">grid_on</i><span class="menu-title" data-i18n="">Profile</span></a></li>
-		<li class="bold"><a class="waves-effect waves-cyan " href="#"><i class="material-icons">grid_on</i><span class="menu-title" data-i18n="">Setting</span></a></li>
+		<li class="bold">
+			<a class="collapsible-header waves-effect waves-cyan " href="#"><i class="material-icons">description</i><span class="menu-title" data-i18n="">Reports</span></a>
+			<div class="collapsible-body">
+				<ul class="collapsible collapsible-sub" data-collapsible="accordion">
+					<li><a class="collapsible-body" href="#" data-i18n=""><i class="material-icons">radio_button_unchecked</i><span>Inventory Report</span></a></li>
+					<li><a class="collapsible-body" href="#" data-i18n=""><i class="material-icons">radio_button_unchecked</i><span> User Report </span></a></li>
+				</ul>
+			</div>
+		</li>
+		<li class="bold"><a class="waves-effect waves-cyan " href="#"><i class="material-icons">settings</i><span class="menu-title" data-i18n="">Profile</span></a></li>
+
+		<li class="bold"><a class="waves-effect waves-cyan " href="#"><i class="material-icons">power_settings_new</i><span class="menu-title" data-i18n="">Logout</span></a></li>
 	</ul>
 	<div class="navigation-background"></div><a class="sidenav-trigger btn-sidenav-toggle btn-floating btn-medium waves-effect waves-light hide-on-large-only" href="#" data-target="slide-out"><i class="material-icons">menu</i></a>
 </aside>
@@ -627,7 +646,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 							</div>
 						</div>
 					</div>
-				</div><
+				</div>
 			</div>
 		</div>
 	</div>
